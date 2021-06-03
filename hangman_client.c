b02015f03980e8a1792b9650070cc52c36efe77a
Mathew Kramsch <mathewkramsch@ucsb.edu>
2021-06-03 14:56:04 -0700

    implemented basic hangman functionality

--- hangman_client.c
+++ hangman_client.c
@@ -10,49 +10,94 @@
 #include <netdb.h>
 #include <stdbool.h>
 
-int main(int argc, char *argv[]) {
+char* getServerMssg(const char *buffer) {
+	printf("server packet: %s\n", buffer);
 
-	// MAKE VARIABLES
+	// server packet -> message to print
+	char *mssg = calloc(256, sizeof(char));
+	if (buffer[0]=='0') {  // if mssg flag is zero
+		char *currentWordGuess = calloc(256, sizeof(char));
+		char *incorrectGuesses = calloc(256, sizeof(char));
+		int wordLength = buffer[1] - '0';
+		int numIncorrect = buffer[2] - '0';
+		for (int i=3; i<3+wordLength; i++) {
+			char letter[50];
+			if (buffer[i]==' ') strcat(currentWordGuess, "_");
+			else {
+				sprintf(letter, "%c", buffer[i]);
+				strcat(currentWordGuess, letter);
+			}
+		}
+		for (int i=3+wordLength; i<3+wordLength+numIncorrect; i++) {
+			char letter[50];
+			sprintf(letter, "%c", buffer[i]);
+			strcat(incorrectGuesses, letter);
+		}
+		strcat(mssg, currentWordGuess);
+		strcat(mssg, "\nIncorrect Guesses: ");
+		strcat(mssg, incorrectGuesses);
+		strcat(mssg, "\n\n");
+	}
+	return mssg;
+}
+
+char* getClientPacket(const char *buffer) {
+	// letter input -> packet to send
+	char *packet = calloc(256, sizeof(char));
+	strcat(packet, buffer);
+	return packet;
+}
+
+void gameRound(int sockfd) {
+	char buffer[256];
+
+	// get serverpacket
+	bzero(buffer,256);
+	read(sockfd,buffer,255);
+
+	// print server message
+	char *serverMssg = getServerMssg(buffer);
+	printf("%s",serverMssg);
+
+	// print prompt for input
+	printf("Letter to guess: ");
+
+	// get input
+	bzero(buffer,256);
+	fgets(buffer,255,stdin);
+
+	// send input
+	char *newBuffer = calloc(256, sizeof(char));
+	newBuffer = getClientPacket(buffer);
+	bzero(buffer,256);
+	memcpy(buffer, newBuffer, strlen(newBuffer));
+	write(sockfd, buffer, strlen(buffer));
+}
+
+int main(int argc, char *argv[]) {
 	int sockfd, portno;  // socket file descriptor & port number
 	struct sockaddr_in serv_addr;
 	struct hostent *server;  // pointer to server's host
 	char buffer[256];
-
-	// CREATE CLIENT SOCKET TO CONNECT TO SERVER
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);  // create socket w/ socket() system call
-
-	// convert IP address passed by user to valid server address
     server = gethostbyname(argv[1]);
-
-	// POPULATE serv_addr STRUCT W/ RELEVANT PARAMS
     bzero((char *) &serv_addr, sizeof(serv_addr));  // clears serv_addr
     serv_addr.sin_family = AF_INET;  // server addr family = IPv4
     bcopy((char *)server->h_addr,  // writes server's address into serv_addr
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);  // get port number specified by client
-
-	// USE SERV_ADDR TO CONNECT TO SERVER
     connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr));  // connect() system call
 
-	// CONNECTION SET UP: CLIENT CAN USE SOCKET TO WRITE/READ
-	while (true) {
-		// WRITE TO SOCKET
-		printf("Send something to server: ");
-		bzero(buffer,256);  // clears buffer
-		fgets(buffer,255,stdin);  // stores user input into buffer
-		write(sockfd,buffer,strlen(buffer));
-
-		// READ FROM SOCKET
-		bzero(buffer,256);
-		read(sockfd,buffer,255);
-		printf("%s",buffer);  // print what server wrote
+	// start game
+	printf("Ready to start game?(y/n): ");
+	bzero(buffer,256);  // clears buffer
+	fgets(buffer,255,stdin);  // stores user input into buffer
+	if (buffer[0]=='y') {
+		write(sockfd,"0",1);
+		while (true) gameRound(sockfd);
 	}
-
-	// CLOSE SOCKET
     close(sockfd);
-
-
 	return 0;
 }
--- hangman_server.c
+++ hangman_server.c
@@ -14,59 +14,107 @@
 
 int i;
 
-char* getMessage(const char *buffer) {
-// PRECONDITION: buffer is non-empty and is all integers
-// POSTCONDITION: returns a char array representing each iterative sum of buffer's digits
-	char *mssg = calloc(256, sizeof(char));
-	// char *tmpBuffer = calloc(256, sizeof(char));
-	// memcpy(tmpBuffer, buffer, strlen(buffer));
-	strcat(mssg, "From server, this is what you said: ");
-	strcat(mssg, buffer);
-	strcat(mssg, "\n");
-	return mssg;
+char* getWord() {
+	// returns random word from file
+	return "mathew";
+} 
+char* getBlankWord(char *word) {
+	// returns a word of blank spaces a long as word
+	char *blankword = calloc(256, sizeof(char));
+	for (int i=0; i<strlen(word); i++) strcat(blankword, " ");
+	return blankword;
+}
+
+bool inWord(char *word, char letter) {
+	// returns true if letter is in word
+	for (int i=0; i<strlen(word); i++)
+		if (word[i]==letter) return true;
+	return false;
+}
+
+void updateCurrentWord(char *word, char *currentWord, char letter) {
+	// replaces " " w/ letter in currentWord everywhere word is
+	for (int i=0; i<strlen(currentWord); i++) {
+		if (word[i]==letter) currentWord[i] = letter;
+	}
+}
+
+char* getServerPacket(char *word, char *currentWord, char *incorrectLetters) {
+	// creates server packet: msg_flag word_length num_incorrect currentWord incorrectLetters
+	char *packet = calloc(256, sizeof(char));
+	char wordLength[50];
+	char numIncorrect[50];
+	strcat(packet, "0");  // message flag
+	sprintf(wordLength, "%lu", strlen(word));  // word length
+	sprintf(numIncorrect, "%lu", strlen(incorrectLetters));  // num incorrect
+	strcat(packet, wordLength);
+	strcat(packet, numIncorrect);
+	strcat(packet, currentWord);  // currentWord
+	strcat(packet, incorrectLetters);
+	return packet;
+}
+
+void gameRound(int newsockfd, char *word, char *currentWord, char *incorrectLetters) {
+	char buffer[256];
+
+	// make serverpacket
+	char *newBuffer = calloc(256, sizeof(char));
+	newBuffer = getServerPacket(word, currentWord, incorrectLetters);
+
+	// send serverpacket
+	bzero(buffer,256);
+	memcpy(buffer, newBuffer, strlen(newBuffer));
+	write(newsockfd, buffer, strlen(buffer));
+
+	// read next letter guess
+	bzero(buffer,256);
+	read(newsockfd,buffer,255);
+	char letterGuess[50];
+	sprintf(letterGuess, "%c", buffer[0]);
+
+	// if correct: add to currentWord
+	if (inWord(word, letterGuess[0])) {
+		updateCurrentWord(word, currentWord, letterGuess[0]);
+	}  // if incorrect: add to incorrectLetters
+	else if (!inWord(incorrectLetters, letterGuess[0]) &&
+				letterGuess[0]!='\n' && letterGuess[0]) {
+		strncat(incorrectLetters,letterGuess,1);
+	}
 }
 
 void *socketThread(void *arg) {
 	i++;
 	int newsockfd = *((int *)arg);
-	char buffer[256];  // 256 bytes
-	int j=0;
-	while (j++<3) {
-		bzero(buffer,256);  // clear buffer
-		read(newsockfd,buffer,255);  // get string from client
-		char *newBuffer = calloc(256, sizeof(char));  // makes new character array
-		newBuffer = getMessage(buffer);  // populate char array with whole message
-		bzero(buffer,256);  // clear buffer
-		memcpy(buffer, newBuffer, strlen(newBuffer));  // overwrites buffer with the message
-		write(newsockfd,buffer,strlen(buffer));  // write overwritten buffer to socket
+	
+	// read initial client message
+	char buffer[256];
+	bzero(buffer,256);
+	read(newsockfd, buffer, 255);
+
+	// start game (if "0")
+	if (buffer[0]=='0') {
+		char *word = getWord();  // random word
+		char *currentWord = getBlankWord(word);
+		char *incorrectLetters = calloc(256, sizeof(char));
+		while (true) gameRound(newsockfd, word, currentWord, incorrectLetters);
 	}
-	close(newsockfd);  // close the client socket after fully handled
+	close(newsockfd);
 	i--;
+	return arg;
 }
 
 int main(int argc, char *argv[]) {
-
-	// MAKE VARIABLES
 	int serverSocketFD, newsockfd, portno;
 	socklen_t clilen;  // socket length variable
-	// char buffer[256];  // 256 bytes
 	struct sockaddr_in serv_addr, cli_addr;  // address structures
 	pthread_t thread;
-
-	// CREATE NEW SOCKET
 	serverSocketFD = socket(AF_INET, SOCK_STREAM, 0);
-
-	// POPULATE serv_addr STRUCTURE
 	bzero((char *) &serv_addr, sizeof(serv_addr));  // clears serv_addr struct
 	portno = atoi(argv[1]);  // get port number
 	serv_addr.sin_family = AF_INET;  // for IPv4
 	serv_addr.sin_addr.s_addr = INADDR_ANY;  // where to accept connections from (all interfaces)
 	serv_addr.sin_port = htons(portno);  // what port to bind to
-
-	// BIND SOCKET TO THE ADDRESS
 	bind(serverSocketFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-
-	// LISTEN ON SOCKET FOR NEW CONNECTIONS
 	listen(serverSocketFD,5);
 
 	i=1;
