6a1b7b2e6f2c51160021b30f7db5e06542e72de6
Mathew Kramsch <mathewkramsch@ucsb.edu>
2021-06-03 06:10:38 -0700

    added boilerplate socket code, client takes input on a loop

--- hangman_client.c
+++ hangman_client.c
@@ -1,1 +1,46 @@
 // hangman_client.c
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+	int sock;
+	unsigned int length;
+	struct sockaddr_in server, from;
+	struct hostent *hp;
+	char buffer[256];
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	server.sin_family = AF_INET;
+	hp = gethostbyname(argv[1]);
+	bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+	server.sin_port = htons(atoi(argv[2]));
+	length=sizeof(struct sockaddr_in);
+	while (true) {
+		// GET INPUT STRING
+		printf("Send something to server: ");
+		bzero(buffer,256);
+		fgets(buffer,256,stdin);
+
+		// WRITE TO SOCKET
+		sendto(sock,buffer,strlen(buffer),0,(const struct sockaddr *)&server,length);  // write to socket
+
+		// READ FROM SOCKET
+		recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);  // read from socket
+
+		// PRINT STRING READ FROM SERVER
+		printf("%s",buffer);
+	}
+
+	// CLOSE THE SOCKET
+	close(sock);
+	return 0;
+}
--- hangman_server.c
+++ hangman_server.c
@@ -1,1 +1,60 @@
 // hangman_server.c
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include<strings.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h> 
+#include <math.h>
+
+char* getMessage(const char *buffer) {
+// PRECONDITION: buffer is non-empty and is all integers
+// POSTCONDITION: returns a char array representing each iterative sum of buffer's digits
+	char *mssg = calloc(256, sizeof(char));
+	// char *tmpBuffer = calloc(256, sizeof(char));
+	// memcpy(tmpBuffer, buffer, strlen(buffer));
+	strcat(mssg, "From server, this is what you said: ");
+	strcat(mssg, buffer);
+	strcat(mssg, "\n");
+	return mssg;
+}
+
+int main(int argc, char *argv[]) {
+
+	// MAKE VARIABLES
+	struct sockaddr_in from;
+	struct sockaddr_in server;
+	socklen_t fromlen;
+	char buff[256];
+
+	// CREATE NEW SOCKET
+	int sock = socket(AF_INET,SOCK_DGRAM,0);
+	server.sin_family = AF_INET;  // for IPv4
+	server.sin_addr.s_addr=INADDR_ANY;  // where to accept connections (all interfaces)
+	server.sin_port=htons(atoi(argv[1]));  // what port to bind to (get from user)
+
+	// BIND SOCKET TO THE ADDRESS
+	bind(sock,(struct sockaddr *)&server,sizeof(server));
+	fromlen = sizeof(struct sockaddr_in);  //length of sockaddr struct pointed to
+
+	// READ & WRITE TO SOCKET
+	while (true) {  // keeps socket open for incoming requests
+		recvfrom(sock,buff,256,0,(struct sockaddr *)&from,&fromlen);  // gets string from client
+		char *newBuffer = calloc(256, sizeof(char));  // makes new character array
+		newBuffer = getMessage(buff);  // populate char array with whole message
+		bzero(buff,256);  // clear buffer
+		memcpy(buff, newBuffer, strlen(newBuffer));  // overwrites buffer with the message
+		sendto(sock,buff,256,0,(struct sockaddr *)&from,fromlen);  // write overwritten buffer to socket
+		bzero(buff,256);  // clear the buffer again
+	}
+
+	// CLOSE THE SOCKET
+	close(sock);
+	return 0;
+}
