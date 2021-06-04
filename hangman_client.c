ea1ce59ad055537e4b39917bb7ba301b62417c14
Mathew Kramsch <mathewkramsch@ucsb.edu>
2021-06-03 17:54:30 -0700

    finished

--- hangman_client.c
+++ hangman_client.c
@@ -11,8 +11,6 @@
 #include <stdbool.h>
 
 char* getServerMssg(const char *buffer) {
-	printf("server packet: %s\n", buffer);
-
 	// server packet -> message to print
 	char *mssg = calloc(256, sizeof(char));
 	if (buffer[0]=='0') {  // if mssg flag is zero
@@ -38,17 +36,35 @@ char* getServerMssg(const char *buffer) {
 		strcat(mssg, incorrectGuesses);
 		strcat(mssg, "\n\n");
 	}
+	else {  // if mssg flag set (to length of message data)
+		char *length = calloc(256, sizeof(char));
+		for (int i=0; !isalpha(buffer[i]); i++) {
+			char digit[50];
+			sprintf(digit, "%c", buffer[i]);
+			strcat(length, digit);
+		}
+		int dataLength = atoi(length);
+		int startIndex = 1;
+		if (dataLength > 0) startIndex = 2;
+		for (int i=startIndex; i<startIndex + dataLength; i++) {
+			char letter[50];
+			sprintf(letter, "%c", buffer[i]);
+			strcat(mssg, letter);
+		}
+		strcat(mssg, "\n");
+	}
 	return mssg;
 }
 
-char* getClientPacket(const char *buffer) {
+char* getClientPacket(char *buffer) {
 	// letter input -> packet to send
 	char *packet = calloc(256, sizeof(char));
+	for (int i=0; buffer[i]; i++) buffer[i] = tolower(buffer[i]);
 	strcat(packet, buffer);
 	return packet;
 }
 
-void gameRound(int sockfd) {
+bool gameRound(int sockfd) {
 	char buffer[256];
 
 	// get serverpacket
@@ -59,19 +75,20 @@ void gameRound(int sockfd) {
 	char *serverMssg = getServerMssg(buffer);
 	printf("%s",serverMssg);
 
-	// print prompt for input
-	printf("Letter to guess: ");
+	if (buffer[0]=='0') {
+		do {
+			printf("Letter to guess: ");
+			bzero(buffer,256);
+			fgets(buffer,255,stdin);
+			if (strlen(buffer)>2 || !isalpha(buffer[0])) printf("Error! Please guess one letter.\n");
+		} while (strlen(buffer)>2 || !isalpha(buffer[0]));
 
-	// get input
-	bzero(buffer,256);
-	fgets(buffer,255,stdin);
-
-	// send input
-	char *newBuffer = calloc(256, sizeof(char));
-	newBuffer = getClientPacket(buffer);
-	bzero(buffer,256);
-	memcpy(buffer, newBuffer, strlen(newBuffer));
-	write(sockfd, buffer, strlen(buffer));
+		char *newBuffer = calloc(256, sizeof(char));
+		bzero(newBuffer, 256);
+		newBuffer = getClientPacket(buffer);
+		write(sockfd, newBuffer, strlen(newBuffer));
+	} else return false;
+	return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -90,13 +107,18 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(portno);  // get port number specified by client
     connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr));  // connect() system call
 
+	bzero(buffer,256);
+	read(sockfd,buffer,255);
+	printf("%s",buffer);
+
 	// start game
-	printf("Ready to start game?(y/n): ");
-	bzero(buffer,256);  // clears buffer
-	fgets(buffer,255,stdin);  // stores user input into buffer
-	if (buffer[0]=='y') {
-		write(sockfd,"0",1);
-		while (true) gameRound(sockfd);
+	if (buffer[0]!='s') {  // if not server-overloaded message
+		bzero(buffer,256);  // clears buffer
+		fgets(buffer,255,stdin);  // stores user input into buffer
+		if (buffer[0]=='y') {
+			write(sockfd,"0",1);
+			while (gameRound(sockfd));
+		}
 	}
     close(sockfd);
 	return 0;
--- hangman_server.c
+++ hangman_server.c
@@ -13,11 +13,32 @@
 #include <pthread.h>
 
 int i;
+int seed;
 
-char* getWord() {
+char* getWord(const char *filename) {
 	// returns random word from file
-	return "mathew";
+	char *chosenWord = calloc(256, sizeof(char));
+	FILE *fd = fopen(filename, "r");
+
+	// count words in file
+	char word[20];
+	int count = 0;
+	while (fgets(word, sizeof word, fd)!=NULL) count++;
+	rewind(fd);
+
+	// store words in array
+	char words[count][20];
+	for (int i=0; i<count; i++) {
+		fgets(word, sizeof word, fd);
+		strcpy(words[i], word);
+	}
+	fclose(fd);
+	int randNum = rand() % count + 1;
+	strcpy(chosenWord, words[randNum]);
+	chosenWord[strlen(chosenWord)-1] = 0;
+	return chosenWord;
 } 
+
 char* getBlankWord(char *word) {
 	// returns a word of blank spaces a long as word
 	char *blankword = calloc(256, sizeof(char));
@@ -54,7 +75,31 @@ char* getServerPacket(char *word, char *currentWord, char *incorrectLetters) {
 	return packet;
 }
 
-void gameRound(int newsockfd, char *word, char *currentWord, char *incorrectLetters) {
+char* getServerPacketMssg(int flag, char *word) {
+	// flag=0: lose, flag=1: win, flag=2: server overloaded
+	char *packet = calloc(256, sizeof(char));
+	if (flag==2) {
+		strcat(packet, "17server-overloaded");
+		return packet;
+	}
+	int length = 26;
+	length += strlen(word);
+	if (flag==0) length +=11;
+	else length += 8;
+	char mssgLength[50];
+	sprintf(mssgLength, "%i", length);
+	strcat(packet, mssgLength);
+	strcat(packet, "The word was ");
+	strcat(packet, word);
+	if (flag==0) strcat(packet, "\nYou Lose :(");
+	if (flag==1) strcat(packet, "\nYou Win!");
+	strcat(packet, "\nGame Over!");
+	return packet;
+
+}
+
+bool gameRound(int newsockfd, char *word, char *currentWord, char *incorrectLetters) {
+	// returns true if game is still going, false otherwise
 	char buffer[256];
 
 	// make serverpacket
@@ -80,24 +125,42 @@ void gameRound(int newsockfd, char *word, char *currentWord, char *incorrectLett
 				letterGuess[0]!='\n' && letterGuess[0]) {
 		strncat(incorrectLetters,letterGuess,1);
 	}
+
+	// check if game is over
+	if (!strcmp(word,currentWord)) {  // if win
+		bzero(newBuffer, 256);
+		newBuffer = getServerPacketMssg(1,word);
+		write(newsockfd, newBuffer, strlen(newBuffer));
+		return false;
+	}
+	if (strlen(incorrectLetters)>=6) {  // if lose
+		bzero(newBuffer, 256);
+		newBuffer = getServerPacketMssg(0,word);
+		write(newsockfd, newBuffer, strlen(newBuffer));
+		return false;
+	}
+	return true;
 }
 
 void *socketThread(void *arg) {
 	i++;
 	int newsockfd = *((int *)arg);
-	
-	// read initial client message
 	char buffer[256];
 	bzero(buffer,256);
-	read(newsockfd, buffer, 255);
+	if (i<4) {
+		write(newsockfd, "Ready to start game?(y/n): ", 27);
 
-	// start game (if "0")
-	if (buffer[0]=='0') {
-		char *word = getWord();  // random word
-		char *currentWord = getBlankWord(word);
-		char *incorrectLetters = calloc(256, sizeof(char));
-		while (true) gameRound(newsockfd, word, currentWord, incorrectLetters);
-	}
+		// read initial client message
+		read(newsockfd, buffer, 255);
+
+		// start game (if "0")
+		if (buffer[0]=='0') {
+			char *word = getWord("hangman_words.txt");  // random word
+			char *currentWord = getBlankWord(word);
+			char *incorrectLetters = calloc(256, sizeof(char));
+			while (gameRound(newsockfd, word, currentWord, incorrectLetters));
+		}
+	} else write(newsockfd, "server-overloaded\n", 18);
 	close(newsockfd);
 	i--;
 	return arg;
@@ -116,13 +179,11 @@ int main(int argc, char *argv[]) {
 	serv_addr.sin_port = htons(portno);  // what port to bind to
 	bind(serverSocketFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
 	listen(serverSocketFD,5);
+	seed = atoi(argv[2]);
+	srand(seed);
 
-	i=1;
+	i=0;
 	while (true) {
-		if (i>3) {
-			printf("too many");
-			break;
-		}
 		clilen = sizeof(cli_addr);
 		newsockfd = accept(serverSocketFD, (struct sockaddr *) &cli_addr, &clilen);
 		pthread_create(&thread, 0, socketThread, &newsockfd);
